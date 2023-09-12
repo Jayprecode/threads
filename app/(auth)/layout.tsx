6f99c9df5cb51bc8972133f9fc083eef7b95bc89
Jayprecode <jayprecode@gmail.com>
2023-09-12 13:15:56 +0100

    (feat): updated layouts

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import React, { ReactNode } from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import '../globals.css'
+
+export const metadata: Metadata = {
+	title: "Threads",
+	description: "A Next.js 13 Meta Threads Application",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={`${inter.className} bg-dark-1`}>{children}</body>
+			</html>
+		</ClerkProvider>
+	);
+};
+
+export default RootLayout;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import "../globals.css";
+import TopBar from "@/components/shared/TopBar";
+import BottomBar from "@/components/shared/BottomBar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Threads",
+	description: "A Next.js 13 Meta Threads Application",
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={inter.className}>
+					<TopBar />
+					<main className="flex flex-row">
+						<LeftSidebar />
+						<section className="main-container">
+							<div className="w-full max-w-4xl">{children}</div>
+						</section>
+						<RightSidebar />
+					</main>
+					<BottomBar />
+				</body>
+			</html>
+		</ClerkProvider>
+	);
+};
+
+export default RootLayout;
